c0f63a39e2910017d03bae07eba2f62463095ede
johnmarkings <33251170+johnmarkings@users.noreply.github.com>
2017-10-31 10:52:41 +0100

    Readded install.sh with executable flag.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Author: John Markings
+# Email: johnmarkings@gmail.com
+
+wget https://github.com/johnmarkings/sysMgmt/raw/master/sysMgmt.so
+wget https://raw.githubusercontent.com/johnmarkings/sysMgmt/master/sysMgmt_ext.py
+sudo mv sysMgmt* /usr/lib/python3/dist-packages/
